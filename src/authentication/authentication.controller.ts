a76d095d96967481d2c21f8e4c65332652f2642f
Le Nguyen Thi <le.nt@qasoftsolution.com>
2022-12-30 11:08:34 +0700

    feat: authencation

--- src/authentication/authentication.controller.ts
+++ src/authentication/authentication.controller.ts
@@ -2,16 +2,9 @@ import {
   Controller,
   Post,
   Body,
-  Req,
-  HttpCode,
-  UseGuards,
-  Res,
 } from '@nestjs/common';
 import { AuthenticationService } from './authentication.service';
 import RegisterDto from './dto/register.dto';
-import { LocalAuthenticationGuard } from './localAuthentication.guard';
-import RequestWithUser from './entities/requestWithUser.entity';
-import { Response } from 'express';
 
 @Controller('authentication')
 export class AuthenticationController {
@@ -21,15 +14,4 @@ export class AuthenticationController {
   async register(@Body() registrationData: RegisterDto) {
     return this.authenticationService.register(registrationData);
   }
-
-  // @HttpCode(200)
-  // @UseGuards(LocalAuthenticationGuard)
-  // @Post('log-in')
-  // async logIn(@Req() request: RequestWithUser, @Res() response: Response) {
-  //   const { user } = request;
-  //   const cookie = this.authenticationService.getCookieWithJwtToken(user.id);
-  //   response.setHeader('Set-Cookie', cookie);
-  //   user.password = undefined;
-  //   return response.send(user);
-  // }
 }
--- src/authentication/authentication.module.ts
+++ src/authentication/authentication.module.ts
@@ -6,7 +6,7 @@ import { LocalStrategy } from './local.strategy';
 import { JwtModule } from '@nestjs/jwt';
 import { jwtConstants } from './constants';
 import { JwtStrategy } from './jwt.strategy';
-import {AuthenticationController} from "./authentication.controller";
+import { AuthenticationController } from './authentication.controller';
 
 @Module({
   imports: [
@@ -16,16 +16,6 @@ import {AuthenticationController} from "./authentication.controller";
       secret: jwtConstants.secret,
       signOptions: { expiresIn: '60s' },
     }),
-    // JwtModule.registerAsync({
-    //   imports: [ConfigModule],
-    //   inject: [ConfigService],
-    //   useFactory: async (configService: ConfigService) => ({
-    //     secret: configService.get('JWT_SECRET'),
-    //     signOptions: {
-    //       expiresIn: `${configService.get('JWT_EXPIRATION_TIME')}s`,
-    //     },
-    //   }),
-    // }),
   ],
   controllers: [AuthenticationController],
   providers: [AuthenticationService, LocalStrategy, JwtStrategy],
